37746d170452ff9398b541d2891678c446115550
Lione <Lione@DESKTOP-GH3LCBM>
2023-02-26 17:52:03 -0700

    Agregar archivos de proyecto.

---
+++ ListasCircularesT/Nodo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasCircularesT
+{
+    internal class Nodo
+    {
+        public Nodo Siguiente;
+        public String dato;
+
+        public Nodo(String dat = "", Nodo siguiente = null)
+        {
+            dato = dat;
+            Siguiente = siguiente;
+        }
+    }
+}
---
+++ ListasCircularesT/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasCircularesT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lista l = new Lista();
+
+        }
+    }
+}
