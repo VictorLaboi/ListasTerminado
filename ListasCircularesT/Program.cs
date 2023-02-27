7102048a0bab48f0efac071318640418765d413a
Lione <Lione@DESKTOP-GH3LCBM>
2023-02-26 18:14:18 -0700

    Se agregan switch case para hacer mas facil la utilizacion de MAIN

--- ListasCircularesT/Program.cs
+++ ListasCircularesT/Program.cs
@@ -11,7 +11,37 @@ namespace ListasCircularesT
         static void Main(string[] args)
         {
             Lista l = new Lista();
-
+            Console.WriteLine("LISTAS SIMPLES CIRCULARES!\nINGRESA OPCION.\nA:INSERTAR DATOS \nB:BUSCAR NODO\nC:ELIMINAR NODO.\nD:MOSTRAR LISTA");
+            char eleccion= (char)Console.Read();
+            switch (eleccion) {
+                case 'A': {
+                        Console.WriteLine("INGRESA EL DATO");
+                        string dat = Console.ReadLine();
+                        l.IniciarNodo(dat);
+                        break;
+                    }
+                case 'B':{
+                        Console.WriteLine("INGRESA NODO A BUSCAR!");
+                        string dat = Console.ReadLine(); 
+                        l.BuscarNodo(dat);
+                        break;
+                    }
+                case 'C':
+                    {
+                        Console.WriteLine("INGRESA EL NODO A ELIMINAR");
+                        string dat = Console.ReadLine();
+                        l.EliminarNodo(dat);
+                        break;
+                    }
+                case 'D': {
+                        l.DespliegueLista();
+                        break;
+                    }
+                default: { 
+                    Console.WriteLine("NOT ALLOWE INSTRUCTION");
+                        break;
+                    }
+            }
         }
     }
 }
