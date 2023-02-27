2dd020ef98741a132b0cc8adf42becf951c84908
Lione <Lione@DESKTOP-GH3LCBM>
2023-02-27 11:53:13 -0700

    FINALIZACION DE PROYECTO EQUIPO

---
+++ ListasSimplementeLigadas/Lista.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasSimplementeLigadas
+{
+    internal class Lista
+    {
+        Nodo nodoInicial;
+        Nodo nodoActual;
+        public Lista()
+        {
+            nodoInicial = new Nodo();
+        }
+        public bool ValidaVacio()
+        {
+            if (nodoInicial.Siguiente == null)
+            {
+                return true;
+            }
+            return false;
+            //return nodoInicial.Siguiente == null;
+
+        }
+        public void VaciarLista()
+        {
+            nodoInicial.Siguiente = null;
+        }
+        public string Recorrer()
+        {
+            string datos = string.Empty;
+            nodoActual = nodoInicial;
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+                datos += nodoActual.Valor + "\n";
+            }
+            return datos;
+        }
+        public void Agregar(string valor)
+        {
+            nodoActual = nodoInicial;
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+            }
+            Nodo nodoNuevo = new Nodo(valor);
+            nodoActual.Siguiente = nodoNuevo;
+        }
+
+        public void AgregarNodoInicio(string valor)
+        {
+            nodoActual = nodoInicial;
+            Nodo nuevoNodo = new Nodo(valor, nodoActual.Siguiente);
+            nodoActual.Siguiente = nuevoNodo;
+        }
+
+        public Nodo Buscar(string valor)
+        {
+            if (ValidaVacio() == false)
+            {
+                Nodo nodoBusqueda = nodoInicial;
+
+                while (nodoBusqueda.Siguiente != null)
+                {
+                    nodoBusqueda = nodoBusqueda.Siguiente;
+
+                    if (nodoBusqueda.Valor == valor)
+                    {
+                        return nodoBusqueda;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public Nodo BuscarPorIndice(int indice)
+        {
+            int Indice = -1;
+
+            if (ValidaVacio() == false)
+            {
+                Nodo nodoBusqueda = nodoInicial;
+
+                while (nodoBusqueda.Siguiente != null)
+                {
+                    nodoBusqueda = nodoBusqueda.Siguiente;
+                    Indice++;
+
+                    if (Indice == indice)
+                    {
+                        return nodoBusqueda;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public Nodo BuscarAnterior(string valor)
+        {
+            if (ValidaVacio() == false)
+            {
+                Nodo nodoBusqueda = nodoInicial;
+
+                while (nodoBusqueda.Siguiente != null
+                            && nodoBusqueda.Siguiente.Valor != valor)
+                {
+                    nodoBusqueda = nodoBusqueda.Siguiente;
+                    if (nodoBusqueda.Siguiente.Valor == valor)
+                    {
+                        return nodoBusqueda;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public void BorrarNodo(string valor)
+        {
+            if (ValidaVacio() == false)
+            {
+                nodoActual = Buscar(valor);
+
+                if (nodoActual != null)
+                {
+                    Nodo nodoAnterior = BuscarAnterior(valor);
+                    nodoAnterior.Siguiente = nodoActual.Siguiente;
+                    nodoActual.Siguiente = null;
+                }
+            }
+        }
+    }
+}
---
+++ ListasSimplementeLigadas/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasSimplementeLigadas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lista l = new Lista();
+            int character = 0;
+            do
+            {
+                Console.WriteLine("\nLISTAS SIMPLES CIRCULARES!\nINGRESA OPCION.\n1:INSERTAR DATOS \n2:BUSCAR NODO\n3:ELIMINAR NODO.\n4:MOSTRAR LISTA\nINGRESA '6' PARA SALIR");
+                character = int.Parse(Console.ReadLine());
+                switch (character)
+                {
+                    case 1:
+                        char op;
+                        int des;
+                        do
+                        {
+                            Console.WriteLine("1:AGREGAR FINAL/2:AGREGAR INICIO");
+                            des = int.Parse(Console.ReadLine());
+                            if (des == 1)
+                            {
+                                Console.WriteLine("INGRESA EL DATO");
+                                string dat = Console.ReadLine();
+                                l.Agregar(dat);
+                            }
+                            else if (des == 2)
+                            {
+                                Console.WriteLine("INGRESA EL DATO");
+                                string dat = Console.ReadLine();
+                                l.AgregarNodoInicio(dat);
+                            }
+                            Console.WriteLine("DESEA TERMINAR? N: NO|| S:SI");
+                            op = Convert.ToChar(Console.ReadLine().ToLower());
+                        } while (op == 'n' || op == 'N');
+                        break;
+                    case 2:
+                        do
+                        {
+                            Console.WriteLine("1:BUSCAR NODO/2:BUSCAR INDICE/3:BUSCAR ANTERIOR A 'X' VALOR ");
+                            des = int.Parse(Console.ReadLine());
+                            if (des == 1)
+                            {
+                                Console.WriteLine("INGRESA NODO A BUSCAR!");
+                                string dat1 = Console.ReadLine();
+                                l.Buscar(dat1);
+                            }
+                            else if (des == 2)
+                            {
+                                Console.WriteLine("INGRESA INDICE A BUSCAR!");
+                                int dat2 = int.Parse(Console.ReadLine());
+                                l.BuscarPorIndice(dat2) ;
+                            }
+                            else if (des == 3)
+                            {
+                                Console.WriteLine("INGRESA INDICE A BUSCAR!");
+                                String dat2 = Console.ReadLine();
+                                l.BuscarAnterior(dat2);
+                            }
+                            else {
+                                Console.WriteLine("OPCION INEXISTENTE!");
+                            }
+                            Console.WriteLine("DESEA TERMINAR? N: NO|| S:SI");
+                            op = Convert.ToChar(Console.ReadLine().ToLower());
+                        } while (op == 'n' || op == 'N');
+                        break;
+                    case 3:
+                        do
+                        {
+                            Console.WriteLine("1:BORRAR POR NODO/2:BORRAR LISTA FULL");
+                            des = int.Parse(Console.ReadLine());
+                            if (des == 1)
+                            {
+                                Console.WriteLine("INGRESE NODO A BORRAR");
+                                String es = (Console.ReadLine());
+                                l.BorrarNodo(es);
+                            }
+                            else if (des == 2)
+                            {
+                                l.VaciarLista();
+                            }
+                            else { Console.WriteLine("OPCION NO DISPONIBLE!"); }
+                            Console.WriteLine("DESEA TERMINAR? N: NO|| S:SI");
+                            op = Convert.ToChar(Console.ReadLine().ToLower());
+                        } while (op == 'n' || op == 'N');
+                        break;
+                    case 4:
+                        l.Recorrer();
+                        break;
+                }
+            } while (character != 6);
+            Console.ReadKey();
+        }
+    }
+}
